e807f15c46f43e494ca57b341cb333bc4d71d979
lawal rashidi <lawalrashidi95@gmail.com>
2022-06-01 14:50:52 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import {createStore} from 'redux'
+import action from "./action";
+
+function App() {
+  
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/action.js
@@ -0,0 +1,19 @@
+
+
+const action = {
+    type: 'INCREMENT'
+}
+
+const counterReducer = (state = 0, action) => {
+    if (action.type === 'INCREMENT') {
+      return state + 1
+    }else if (action.type === 'DECREMENT') {
+      return state - 1 
+    }else if (action.type === 'ZERO') {
+      return 0
+    }
+
+    return state
+  }
+
+ 
